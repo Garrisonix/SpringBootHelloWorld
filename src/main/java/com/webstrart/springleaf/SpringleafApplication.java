e1dce098150d1e5af63d15d718f4b9a4ec0594d7
Garrisnix <15367876+Garrisonix@users.noreply.github.com>
2018-03-25 20:19:19 +0100

    Initial Commit

---
+++ src/main/java/com/webstrart/springleaf/SpringleafApplication.java
@@ -0,0 +1,12 @@
+package com.webstrart.springleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringleafApplication.class, args);
+    }
+}
---
+++ src/test/java/com/webstrart/springleaf/SpringleafApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webstrart.springleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringleafApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
